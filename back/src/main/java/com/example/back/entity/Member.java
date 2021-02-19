d6edf326c263522a63767685a7602462a8da6420
RYUSEULGI <rhythm296@naver.com>
2021-02-19 19:07:05 +0900

    nav 연결

---
+++ back/src/main/java/com/example/back/entity/Member.java
@@ -0,0 +1,12 @@
+package com.example.back.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private String userEmail;
+    private String userPw;
+    private String userPhoneNumber;
+}
