739922091f52947405417c2b260aa517d4292a91
RYUSEULGI <rhythm296@naver.com>
2021-03-03 12:14:15 +0900

    feat : navbar 추가

--- front/src/views/Index.vue
+++ front/src/views/Index.vue
@@ -1,21 +1,20 @@
 <template>
-  <NavBar>
     <v-container>
-      <h2>사이트이름</h2>
-      <div class="wrap">
+      <NavBar class="navbar"/>
+        <h2>사이트이름</h2>
+        <div class="wrap">
 
-        <h3>검색할 지역을 선택해주세요</h3>
-        <toggle-item></toggle-item>
-        <toggle-item></toggle-item>
-        <button type="submit" class="search-btn" disabled>검색</button>
+          <h3>검색할 지역을 선택해주세요</h3>
+          <toggle-item></toggle-item>
+          <toggle-item></toggle-item>
+          <button type="submit" class="search-btn" disabled>검색</button>
 
-        <h3>최다 검색 위치</h3>
-        <search-box>{{ }}</search-box>
-        <h3>최근 나의 검색</h3>
-        <search-box>{{ }}</search-box>
-      </div>
+          <h3>최다 검색 위치</h3>
+          <search-box></search-box>
+          <h3>최근 나의 검색</h3>
+          <search-box></search-box>
+        </div>
     </v-container>
-  </NavBar>
 </template>
 
 
@@ -36,6 +35,12 @@ export default {
 
 <style src="@/assets/style/common.css"></style>
 <style scoped>
+.navbar{
+  position: absolute;
+  top: 4%;
+  left: 3%;
+}
+
 .search-btn {
     width: 10%;
     height: 3rem;
