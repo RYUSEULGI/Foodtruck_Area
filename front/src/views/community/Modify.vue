d61ba949aefebed51a30db781fca366eb083bf28
RYUSEULGI <rhythm296@naver.com>
2021-03-03 12:19:09 +0900

    remove : vue 기본 파일 삭제

---
+++ front/src/views/community/Modify.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="body__container">
+        <div class="inner">
+
+            <h2>글수정</h2>
+
+            <section class="section__writeNew wrap">
+                <write-form @submit="onSubmit"/>
+            </section>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import WriteForm from '@/components/community/WriteForm'
+import { mapState } from 'vuex'
+    export default {
+        name: 'Modify',
+        components: {
+            WriteForm
+        },
+        props: {
+            listNo: {
+                type: String,
+                required: true
+            }
+        },
+        computed: {
+            ...mapState(['community'])
+        },
+        created() {
+            console.log('수정완료');
+            this.fetchCommunityList(this.listNo)
+            .catch( () => {
+                this.$router.back()
+            });
+        },
+        methods: {
+            onSubmit(){
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
